2e485bd9c44f41a1485a59627bcb6ffa3e45a844
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-17 20:23:45 -0500

    set up mongodb connection, working smoothly

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+//set up dotenv, (npm i mongoosed dotenv)
+const dotenv = require('dotenv');
+dotenv.config();
+
 //load express
 const express = require("express");
 
+//set up mongoose
+const mongoose = require('mongoose');
+
 const app = express();
+
+
+//Connect to MongoDB using he connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+//log this connecttion status to the console to ensure connection is running smoothly and without error.
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}, eureka!`)
+})
 
 //building first route/home page
 //GET /

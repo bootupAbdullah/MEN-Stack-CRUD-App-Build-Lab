37583eb255a38f266015926464378126cdd08f3e
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-17 20:11:23 -0500

    added .gitignore, .env

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 
 const app = express();
 
+//building first route/home page
+//GET /
+app.get('/', async (req, res) =>{ 
+    res.render("index.ejs");
+});
+
+
+
+
+
 app.listen(3001, () => {
     console.log("Listening on port 3001");
 });
